8660f9639e0a9eb0ccb65da9193965b6bfefb7d3
MohammedAyman2018 <emidobibo20@gmail.com>
2021-09-27 13:57:19 +0200

    feat: finish CRUD categories.

---
+++ public/javascripts/categories.js
@@ -0,0 +1,93 @@
+const cateTable = document.querySelector('tbody')
+const newCateButton = document.getElementById('newCateButton')
+
+newCateButton.addEventListener('click', addCategory)
+
+async function getCates() {
+  const data = await axios.get('/api/categories/').catch(err => alert(err))
+  renderCates(data.data)
+}
+
+async function renderCates (cats) {
+  cateTable.innerHTML = ''
+
+  for (let i = 0; i < cats.length; i++) {
+    const cate = cats[i];
+    const tr = await createNewRow(cate)
+    cateTable.appendChild(tr)   
+  }
+}
+
+function createNewRow (cate) {
+  const tr = document.createElement('tr')
+
+  // This row will be idx no 
+  const trNo = document.querySelectorAll('tr').length 
+
+  // Create name and created At
+  const name = document.createElement('td')
+  const createdAt = document.createElement('td')
+  name.innerText = cate.name
+  createdAt.innerText = cate.createdAt.substr(0, 10)
+  
+  // Icons
+  const operations = document.createElement('td')
+  const deleteIcon = document.createElement('i')
+  deleteIcon.classList.add("ri-delete-bin-line", "text-red-800", "mx-4", "cursor-pointer")
+  const editIcon = document.createElement('i')
+  editIcon.classList.add("ri-edit-line", "text-yellow-800", "cursor-pointer")
+
+  deleteIcon.addEventListener('click', deleteCategory)
+  editIcon.addEventListener('click', editCategory)
+
+  deleteIcon.setAttribute('data-category', cate._id)
+  editIcon.setAttribute('data-category', cate._id)
+
+  deleteIcon.setAttribute('data-idx', trNo)
+  editIcon.setAttribute('data-idx', trNo)
+
+  operations.appendChild(deleteIcon)
+  operations.appendChild(editIcon)
+
+
+  name.classList.add('text-center', "border", "border-green-600")
+  createdAt.classList.add('text-center', "border", "border-green-600")
+  operations.classList.add('text-center', "border", "border-green-600")
+  tr.appendChild(name)
+  tr.appendChild(createdAt)
+  tr.appendChild(operations)
+  return tr
+}
+
+async function addCategory() {
+  const name = prompt('Enter new category text...')
+  if (name) {
+    const res = await axios.post('/api/categories/', { name }).catch(err => alert(err))
+    const tr = createNewRow(res.data)
+    cateTable.appendChild(tr)
+  }
+}
+
+async function deleteCategory(e) {
+  const youSure = confirm('You really want to delete this category? You can\'t undo this.')
+  if (youSure) {
+    const _id = e.target.getAttribute('data-category')
+    const tr = e.target.parentElement.parentElement
+    console.log(tr)
+    const res = await axios.delete(`/api/categories/${_id}`).catch(err => alert(err))
+    tr.remove()
+  }
+}
+
+async function editCategory(e) {
+  const name = prompt('Enter new category name...')
+  if (name) {
+    const _id = e.target.getAttribute('data-category')
+    const tr = e.target.parentElement.previousElementSibling.previousElementSibling
+    console.log(tr)
+    const res = await axios.patch(`/api/categories/${_id}`, { name }).catch(err => alert(err))
+    tr.innerText = name
+  }
+}
+
+getCates()
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,18 @@ router.get('/article/:title', function(req, res, next) {
   res.render('related.jade');
 });
 
+// Admin Routes
+router.get('/admin/articles', function (req, res, next) {
+  res.render('admin/articles/index.jade')
+})
+
+router.get('/admin/articles/add', function (req, res, next) {
+  res.render('admin/articles/add.jade')
+})
+
+router.get('/admin/categories', function (req, res, next) {
+  res.render('admin/categories/index.jade')
+})
+
+
 module.exports = router;
